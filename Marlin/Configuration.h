b8d63d2984eac6be3ebde47a96cb66922fdc8fe9
robbycandra <robbycandra.mail@gmail.com>
2019-04-18 07:45:29 +0700

    Conf: Use Probe Manually

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -808,8 +808,8 @@
  * Use G29 repeatedly, adjusting the Z height at each point with movement commands
  * or (with LCD_BED_LEVELING) the LCD controller.
  */
-//#define PROBE_MANUALLY
-//#define MANUAL_PROBE_START_Z 0.2
+#define PROBE_MANUALLY
+#define MANUAL_PROBE_START_Z 0
 
 /**
  * A Fix-Mounted Probe either doesn't deploy or needs manual deployment.
