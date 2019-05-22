98ea69945993896fff095b784d80fdff2b431b47
robbycandra <robbycandra.mail@gmail.com>
2019-05-23 06:54:14 +0700

    Config Acceleration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -723,7 +723,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 5000 }
+#define DEFAULT_MAX_ACCELERATION      { 2000, 2000, 100, 5000 }
 #define MAX_ACCELERATION_LIMIT        { 3000, 3000, 200, 10000 }
 
 /**
@@ -734,7 +734,7 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          1500    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_ACCELERATION          1000    // X, Y, Z and E acceleration for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  1500    // E acceleration for retracts
 #define DEFAULT_TRAVEL_ACCELERATION   1500    // X, Y, Z acceleration for travel (non printing) moves
 
