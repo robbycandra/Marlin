90c9ced91b27575cf736ab5e3ba1dbd01c40e2f2
robbycandra <robbycandra.mail@gmail.com>
2019-07-14 18:16:20 +0700

    Change EndScript Park Nozzle

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -914,7 +914,7 @@
 
   //#define MENU_ADDAUTOSTART               // Add a menu option to run auto#.g files
 
-  #define EVENT_GCODE_SD_STOP "G27"       // G-code to run on Stop Print (e.g., "G28XY" or "G27")
+  #define EVENT_GCODE_SD_STOP "G27 P2"       // G-code to run on Stop Print (e.g., "G28XY" or "G27")
 
   /**
    * Continue after Power-Loss (Creality3D)
