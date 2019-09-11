be5b822b0c9d64901be9df2ec66fc90e405911de
robbycandra <robbycandra.mail@gmail.com>
2019-09-12 00:34:33 +0700

    Change D2 Pin

--- Marlin/src/pins/stm32/pins_REXYZ_DLION_D2.h
+++ Marlin/src/pins/stm32/pins_REXYZ_DLION_D2.h
@@ -50,51 +50,53 @@
 //
 // Limit Switches
 //
-#define X_STOP_PIN         PC6
-#define Y_STOP_PIN         PG8
-#define Z_STOP_PIN         PG7
-//#define X_MAX_PIN          PC5
-//#define Y_MAX_PIN          PC4
-//#define Z_MAX_PIN          PB0
+#define X_STOP_PIN         PD6  //PIN 122 - PC6
+#define Y_STOP_PIN         PG9  //PIN 124 - PG8
+#define Z_STOP_PIN         PG10 //PIN 125 - PG7
+//#define X_MAX_PIN          PG15 //PIN 132 - PC5
+//#define Y_MAX_PIN          PG14 //PIN 129 - PC4
+//#define Z_MAX_PIN          PG13 //PIN 128 - PB0
+
+#define FIL_RUNOUT_PIN     PG11 //PIN 126 - PC7
+#define EX_IO_PIN          PB8  //PIN 139 
 
 //
 // Steppers
 //
-#define X_STEP_PIN         PD6
-#define X_DIR_PIN          PD3
-#define X_ENABLE_PIN       PG9
+#define X_STEP_PIN         PD12 //PD6
+#define X_DIR_PIN          PD11 //PD3
+#define X_ENABLE_PIN       PD13 //PG9
 
-#define Y_STEP_PIN         PG12
-#define Y_DIR_PIN          PG11
-#define Y_ENABLE_PIN       PG13
+#define Y_STEP_PIN         PG3 //PG12
+#define Y_DIR_PIN          PG2 //PG11
+#define Y_ENABLE_PIN       PG4 //PG13
 
-#define Z_STEP_PIN         PG15
-#define Z_DIR_PIN          PG14
-#define Z_ENABLE_PIN       PB8
+#define Z_STEP_PIN         PG6 //PG15
+#define Z_DIR_PIN          PG5 //PG14
+#define Z_ENABLE_PIN       PG7 //PB8
 
-#define E0_STEP_PIN        PE2
-#define E0_DIR_PIN         PB9
-#define E0_ENABLE_PIN      PE3
+#define E0_STEP_PIN        PC6 //PE2
+#define E0_DIR_PIN         PG8 //PB9
+#define E0_ENABLE_PIN      PC7 //PE3
 
-#define E1_STEP_PIN        PE5
-#define E1_DIR_PIN         PC3 //PE4
-#define E1_ENABLE_PIN      PE6
+//#define E1_STEP_PIN        //PE5
+//#define E1_DIR_PIN         //PC3 //PE4
+//#define E1_ENABLE_PIN      //PE6
 
 //
 // Temperature Sensors
 //
-#define TEMP_0_PIN         PC2
-#define TEMP_BED_PIN       PC1
+#define TEMP_0_PIN         PC1 // PF6 // PC2
+#define TEMP_BED_PIN       PC0 // PC1
 
 //
 // Heaters / Fans
 //
-#define HEATER_0_PIN       PA2
-#define HEATER_BED_PIN     PA3
+#define HEATER_0_PIN       PB0 //PA2
+#define HEATER_BED_PIN     PB1 //PA3
 
-#define FAN_PIN            PA1
+#define FAN_PIN            PA1 //
 
-#define FIL_RUNOUT_PIN     PC7
 
 //
 // LCD
@@ -110,13 +112,29 @@
 //
 // SD Card
 //
-#define SD_DETECT_PIN      PF10
+//#define SD_DETECT_PIN     PC2 
+//#define SCK_PIN           PC12
+//#define MISO_PIN          PB8 
+//#define MOSI_PIN          PD2
+//#define SS_PIN            PC11
 
 //
 // Misc.
 //
-#define BEEPER_PIN         PE4 //PC3    // use PB7 to shut up if desired
-#define LED_PIN            PC13
+#define BEEPER_PIN         PE4  //Rexyz PC3    // use PB7 to shut up if desired
+
+/**
+ * STM32F1 Default SPI Pins
+ *
+ *         SS     SCK     MISO    MOSI
+ *       +-----------------------------+
+ *  SPI1 | PA4    PA5     PA6     PA7  |
+ *  SPI2 | PB12   PB13    PB14    PB15 |
+ *  SPI3 | PA15   PB3     PB4     PB5  |
+ *       +-----------------------------+
+ * Any pin can be used for Chip Select (SS_PIN)
+ * SPI1 is enabled by default
+ */
 
 //
 // Touch support
@@ -124,4 +142,7 @@
 #if ENABLED(TOUCH_BUTTONS)
   #define TOUCH_CS_PIN     PA4
   #define TOUCH_INT_PIN    PC4
+  #define TOUCH_MISO_PIN   PA6
+  #define TOUCH_MOSI_PIN   PA7
+  #define TOUCH_SCK_PIN    PA5
 #endif
